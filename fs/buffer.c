a783afadd057ef34a38fa795ecb78f658fc3ed9f
Sanglok99 <contactlogi@gmail.com>
2025-09-24 17:37:56 +0000

    add __my_mark_inode_dirty()

--- fs/buffer.c
+++ fs/buffer.c
@@ -53,6 +53,7 @@ extern void folio_memcg_unlock(struct folio *folio);
 extern bool node_set_mark(struct xa_node *node, unsigned int offset, xa_mark_t mark);
 extern void folio_account_dirtied(struct folio *folio, struct address_space *mapping);
 extern void xa_mark_set(struct xarray *xa, xa_mark_t mark);
+extern void wb_wakeup_delayed(struct bdi_writeback *wb);
 
 /*
  * Per-cpu buffer LRU implementation.  To reduce the cost of __find_get_block().
@@ -745,6 +746,391 @@ void __my_folio_mark_dirty(struct folio *folio, struct address_space *mapping,
 }
 
 /**
+ * locked_inode_to_wb_and_lock_list - determine a locked inode's wb and lock it
+ * @inode: inode of interest with i_lock held
+ *
+ * Returns @inode's wb with its list_lock held.  @inode->i_lock must be
+ * held on entry and is released on return.  The returned wb is guaranteed
+ * to stay @inode's associated wb until its list_lock is released.
+ */
+static struct bdi_writeback *
+my_locked_inode_to_wb_and_lock_list(struct inode *inode)
+    __releases(&inode->i_lock)
+    __acquires(&wb->list_lock)
+{
+    while (true) {
+        struct bdi_writeback *wb = inode_to_wb(inode);
+
+        printk("[%s]: 0\n", __func__); // test code
+
+        /*
+         * inode_to_wb() association is protected by both
+         * @inode->i_lock and @wb->list_lock but list_lock nests
+         * outside i_lock.  Drop i_lock and verify that the
+         * association hasn't changed after acquiring list_lock.
+         */
+        wb_get(wb);
+        spin_unlock(&inode->i_lock);
+        spin_lock(&wb->list_lock);
+
+        /* i_wb may have changed inbetween, can't use inode_to_wb() */
+        if (likely(wb == inode->i_wb)) {
+            printk("[%s]: 1\n", __func__); // test code
+            
+            wb_put(wb); /* @inode already has ref */
+            return wb;
+        }
+        printk("[%s]: 2\n", __func__); // test code
+
+        spin_unlock(&wb->list_lock);
+        wb_put(wb);
+        cpu_relax();
+        spin_lock(&inode->i_lock);
+    }
+}
+
+static bool my_wb_io_lists_populated(struct bdi_writeback *wb)
+{
+    if (wb_has_dirty_io(wb)) {
+        printk("[%s]: 0\n", __func__); // test code
+        
+        return false;
+    } else {
+        printk("[%s]: 1\n", __func__); // test code
+        printk("[%s]: WB_has_dirty_io= %d\n", __func__, WB_has_dirty_io); // test code
+        
+        printk("[%s]: wb->state(before set_bit)= %lu\n", __func__, wb->state); // test code
+
+        set_bit(WB_has_dirty_io, &wb->state);
+        
+        printk("[%s]: wb->state(after set_bit)= %lu\n", __func__, wb->state); // test code
+        
+        WARN_ON_ONCE(!wb->avg_write_bandwidth);
+        atomic_long_add(wb->avg_write_bandwidth,
+                &wb->bdi->tot_write_bandwidth);
+        return true;
+    }
+}
+
+static void my_wb_io_lists_depopulated(struct bdi_writeback *wb)
+{
+    if (wb_has_dirty_io(wb) && list_empty(&wb->b_dirty) &&
+        list_empty(&wb->b_io) && list_empty(&wb->b_more_io)) {
+        printk("[%s]: 0\n", __func__); // test code
+        
+        clear_bit(WB_has_dirty_io, &wb->state);
+        WARN_ON_ONCE(atomic_long_sub_return(wb->avg_write_bandwidth,
+                    &wb->bdi->tot_write_bandwidth) < 0);
+    }
+    printk("[%s]: 1\n", __func__); // test code
+}
+
+/**
+ * inode_io_list_move_locked - move an inode onto a bdi_writeback IO list
+ * @inode: inode to be moved
+ * @wb: target bdi_writeback
+ * @head: one of @wb->b_{dirty|io|more_io|dirty_time}
+ *
+ * Move @inode->i_io_list to @list of @wb and set %WB_has_dirty_io.
+ * Returns %true if @inode is the first occupant of the !dirty_time IO
+ * lists; otherwise, %false.
+ */
+static bool my_inode_io_list_move_locked(struct inode *inode,
+                      struct bdi_writeback *wb,
+                      struct list_head *head)
+{
+    assert_spin_locked(&wb->list_lock);
+    assert_spin_locked(&inode->i_lock);
+    WARN_ON_ONCE(inode->i_state & I_FREEING);
+
+    list_move(&inode->i_io_list, head);
+    
+    printk("[%s]: 0\n", __func__); // test code
+
+    /* dirty_time doesn't count as dirty_io until expiration */
+    if (head != &wb->b_dirty_time) {
+        printk("[%s]: 1\n", __func__); // test code
+        return my_wb_io_lists_populated(wb);
+    }
+    printk("[%s]: 2\n", __func__); // test code
+
+    my_wb_io_lists_depopulated(wb);
+    return false;
+}
+
+/**
+ * __mark_inode_dirty - internal function to mark an inode dirty
+ *
+ * @inode: inode to mark
+ * @flags: what kind of dirty, e.g. I_DIRTY_SYNC.  This can be a combination of
+ *     multiple I_DIRTY_* flags, except that I_DIRTY_TIME can't be combined
+ *     with I_DIRTY_PAGES.
+ *
+ * Mark an inode as dirty.  We notify the filesystem, then update the inode's
+ * dirty flags.  Then, if needed we add the inode to the appropriate dirty list.
+ *
+ * Most callers should use mark_inode_dirty() or mark_inode_dirty_sync()
+ * instead of calling this directly.
+ *
+ * CAREFUL!  We only add the inode to the dirty list if it is hashed or if it
+ * refers to a blockdev.  Unhashed inodes will never be added to the dirty list
+ * even if they are later hashed, as they will have been marked dirty already.
+ *
+ * In short, ensure you hash any inodes _before_ you start marking them dirty.
+ *
+ * Note that for blockdevs, inode->dirtied_when represents the dirtying time of
+ * the block-special inode (/dev/hda1) itself.  And the ->dirtied_when field of
+ * the kernel-internal blockdev inode represents the dirtying time of the
+ * blockdev's pages.  This is why for I_DIRTY_PAGES we always use
+ * page->mapping->host, so the page-dirtying time is recorded in the internal
+ * blockdev inode.
+ */
+void __my_mark_inode_dirty(struct inode *inode, int flags)
+{
+    struct super_block *sb = inode->i_sb;
+    int dirtytime = 0;
+    struct bdi_writeback *wb = NULL;
+
+    printk("[%s]: 0\n", __func__); // test code
+    printk("[%s]: flags= %d\n", __func__, flags); // test code
+    printk("[%s]: I_DIRTY_INODE= %d\n", __func__, I_DIRTY_INODE); // test code 
+    printk("[%s]: I_DIRTY_TIME= %d\n", __func__, I_DIRTY_TIME); // test code 
+
+    // trace_writeback_mark_inode_dirty(inode, flags);
+
+    if (flags & I_DIRTY_INODE) {
+        printk("[%s]: 1\n", __func__); // test code
+        /*
+         * Inode timestamp update will piggback on this dirtying.
+         * We tell ->dirty_inode callback that timestamps need to
+         * be updated by setting I_DIRTY_TIME in flags.
+         */
+        if (inode->i_state & I_DIRTY_TIME) {
+            printk("[%s]: 2\n", __func__); // test code
+            spin_lock(&inode->i_lock);
+            if (inode->i_state & I_DIRTY_TIME) {
+                printk("[%s]: 3\n", __func__); // test code
+                inode->i_state &= ~I_DIRTY_TIME;
+                flags |= I_DIRTY_TIME;
+            }
+            printk("[%s]: 4\n", __func__); // test code
+            spin_unlock(&inode->i_lock);
+        }
+
+        printk("[%s]: 5\n", __func__); // test code
+
+        /*
+         * Notify the filesystem about the inode being dirtied, so that
+         * (if needed) it can update on-disk fields and journal the
+         * inode.  This is only needed when the inode itself is being
+         * dirtied now.  I.e. it's only needed for I_DIRTY_INODE, not
+         * for just I_DIRTY_PAGES or I_DIRTY_TIME.
+         */
+        // trace_writeback_dirty_inode_start(inode, flags);
+        if (sb->s_op->dirty_inode) {
+            printk("[%s]: 6\n", __func__); // test code
+            sb->s_op->dirty_inode(inode,
+                flags & (I_DIRTY_INODE | I_DIRTY_TIME));
+        }
+
+        printk("[%s]: 7\n", __func__); // test code
+                                      
+        // trace_writeback_dirty_inode(inode, flags);
+
+        /* I_DIRTY_INODE supersedes I_DIRTY_TIME. */
+        flags &= ~I_DIRTY_TIME;
+    } else {
+        /*
+         * Else it's either I_DIRTY_PAGES, I_DIRTY_TIME, or nothing.
+         * (We don't support setting both I_DIRTY_PAGES and I_DIRTY_TIME
+         * in one call to __mark_inode_dirty().)
+         */
+
+        printk("[%s]: 8\n", __func__); // test code
+        
+        dirtytime = flags & I_DIRTY_TIME;
+        
+        printk("[%s]: dirtytime= %d\n", __func__, dirtytime); // test code
+
+        WARN_ON_ONCE(dirtytime && flags != I_DIRTY_TIME);
+    }
+    
+    printk("[%s]: 9\n", __func__); // test code
+
+    /*
+     * Paired with smp_mb() in __writeback_single_inode() for the
+     * following lockless i_state test.  See there for details.
+     */
+    smp_mb();
+
+    if ((inode->i_state & flags) == flags) {
+        printk("[%s]: 10\n", __func__); // test code
+        return;
+    }
+
+    printk("[%s]: 11\n", __func__); // test code
+
+    spin_lock(&inode->i_lock);
+    if ((inode->i_state & flags) != flags) {
+        
+        printk("[%s]: 12\n", __func__); // test code
+                                     
+        const int was_dirty = inode->i_state & I_DIRTY;
+
+        printk("[%s]: inode->i_state= %lu\n", __func__, inode->i_state); // test code
+        printk("[%s]: I_DIRTY= %d\n", __func__, I_DIRTY); // test code
+        printk("[%s]: was_dirty= %d\n", __func__, was_dirty); // test code
+
+        /*
+        // === test code begin ===
+        if(inode->i_wb) {
+            printk("[%s]: node->i_wb= %p\n", __func__, inode->i_wb);
+        } else {
+            printk("[%s]: inode->i_wb is NULL\n", __func__);
+        }
+        // === test code end ===
+        */
+            
+        inode_attach_wb(inode, NULL);
+
+        /*
+        // === test code begin ===
+        if(inode->i_wb) {
+            printk("[%s]: node->i_wb= %p\n", __func__, inode->i_wb);
+        } else {
+            printk("[%s]: inode->i_wb is NULL\n", __func__);
+        }
+        // === test code end ===
+        */
+
+        inode->i_state |= flags;
+
+        printk("[%s]: inode->i_state(after '|= flags;')= %lu\n", __func__, inode->i_state); // test code
+
+        /*
+         * Grab inode's wb early because it requires dropping i_lock and we
+         * need to make sure following checks happen atomically with dirty
+         * list handling so that we don't move inodes under flush worker's
+         * hands.
+         */
+        if (!was_dirty) {
+
+            printk("[%s]: 13\n", __func__); // test code
+                                         
+            wb = my_locked_inode_to_wb_and_lock_list(inode);
+            spin_lock(&inode->i_lock);
+        }
+
+        printk("[%s]: 14\n", __func__); // test code
+
+        /*
+         * If the inode is queued for writeback by flush worker, just
+         * update its dirty state. Once the flush worker is done with
+         * the inode it will place it on the appropriate superblock
+         * list, based upon its state.
+         */
+        if (inode->i_state & I_SYNC_QUEUED) {
+
+            printk("[%s]: 15\n", __func__); // test code
+                                          
+            goto out_unlock;
+        }
+    
+        printk("[%s]: 16\n", __func__); // test code
+
+        /*
+         * Only add valid (hashed) inodes to the superblock's
+         * dirty list.  Add blockdev inodes as well.
+         */
+        if (!S_ISBLK(inode->i_mode)) {
+            
+            printk("[%s]: 17\n", __func__); // test code
+
+            if (inode_unhashed(inode)) {
+
+                printk("[%s]: 18\n", __func__); // test code
+                                              
+                goto out_unlock;
+            }
+        }
+        
+        printk("[%s]: 19\n", __func__); // test code
+
+        if (inode->i_state & I_FREEING) {
+            printk("[%s]: 20\n", __func__); // test code
+            goto out_unlock;
+        }
+
+        printk("[%s]: 21\n", __func__); // test code
+
+        /*
+         * If the inode was already on b_dirty/b_io/b_more_io, don't
+         * reposition it (that would break b_dirty time-ordering).
+         */
+        if (!was_dirty) {
+            struct list_head *dirty_list;
+            bool wakeup_bdi = false;
+
+            printk("[%s]: 22\n", __func__); // test code
+
+            inode->dirtied_when = jiffies;
+            if (dirtytime) {
+                printk("[%s]: 23\n", __func__); // test code
+                inode->dirtied_time_when = jiffies;
+            }
+            
+            printk("[%s]: 24\n", __func__); // test code
+
+            if (inode->i_state & I_DIRTY) {
+                printk("[%s]: 25\n", __func__); // test code
+                dirty_list = &wb->b_dirty;
+            }
+            else {
+                printk("[%s]: 26\n", __func__); // test code
+                dirty_list = &wb->b_dirty_time;
+            }
+
+            printk("[%s]: 27\n", __func__); // test code
+                                  
+            wakeup_bdi = my_inode_io_list_move_locked(inode, wb,
+                                   dirty_list);
+
+            spin_unlock(&wb->list_lock);
+            spin_unlock(&inode->i_lock);
+            // trace_writeback_dirty_inode_enqueue(inode);
+
+            /*
+             * If this is the first dirty inode for this bdi,
+             * we have to wake-up the corresponding bdi thread
+             * to make sure background write-back happens
+             * later.
+             */
+            if (wakeup_bdi &&
+                (wb->bdi->capabilities & BDI_CAP_WRITEBACK)) {
+                printk("[%s]: 28\n", __func__); // test code
+                wb_wakeup_delayed(wb);
+            }
+            
+            printk("[%s]: 29\n", __func__); // test code
+            
+            return;
+        }
+    }
+out_unlock:
+    
+    printk("[%s]: 29\n", __func__); // test code
+
+    if (wb) {
+        printk("[%s]: 30\n", __func__); // test code
+        spin_unlock(&wb->list_lock);
+    }
+
+    printk("[%s]: 31\n", __func__); // test code
+    
+    spin_unlock(&inode->i_lock);
+}
+
+/**
  * mark_buffer_dirty - mark a buffer_head as needing writeout
  * @bh: the buffer_head to mark dirty
  *
@@ -834,7 +1220,18 @@ void my_mark_buffer_dirty(struct buffer_head *bh)
         folio_memcg_unlock(folio);
         if (mapping) {
             printk("[%s]: 7\n", __func__);
-            __mark_inode_dirty(mapping->host, I_DIRTY_PAGES);
+
+            // === test code begin ===
+            if(mapping->host) {
+                printk("[%s]: mapping->host->i_ino=%lu\n", __func__, mapping->host->i_ino);
+                printk("[%s]: mapping->host->i_state=%lu\n", __func__, mapping->host->i_state);
+                
+            } else {
+                printk("[%s]: mapping->host is NULL\n", __func__);
+            }
+            // === test code end ===
+
+            __my_mark_inode_dirty(mapping->host, I_DIRTY_PAGES);
         }
     }
 }
